03cc1fad3f1bc8bb0bfb4fe636b8a843ecf5378b
James Valaitis <james@infinityjames.com>
2013-04-14 15:22:42 +0100

    Initial Commit

---
+++ MakeAMealOfIt/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MakeAMealOfIt
+//
+//  Created by James Valaitis on 14/04/2013.
+//  Copyright (c) 2013 &Beyond. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MakeAMealOfIt/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MakeAMealOfIt
+//
+//  Created by James Valaitis on 14/04/2013.
+//  Copyright (c) 2013 &Beyond. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
