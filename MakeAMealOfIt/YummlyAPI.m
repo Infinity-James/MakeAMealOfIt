c848b90c8e4b3092a62781909dfc083ce1133d71
James Valaitis <james@infinityjames.com>
2014-04-18 15:16:02 +0100

    Separated out the API keys.

--- MakeAMealOfIt/YummlyAPI.m
+++ MakeAMealOfIt/YummlyAPI.m
@@ -10,11 +10,10 @@
 
 #pragma mark - Constants & Static Variables: URL Request Strings
 
-static NSString *const kYummlyAppID						= @"286bc6f3";
+#import "YummlyAPIKeys.h"
+
 static NSString *const kYummlyAppIDHeaderField			= @"X-Yummly-App-ID";
-static NSString *const kYummlyAppKey					= @"a175c2748234fcb57b0e4594ea95f3d7";
 static NSString *const kYummlyAppKeyHeaderField			= @"X-Yummly-App-Key";
-static NSString *const kYummlyAuthorisationURLExtension	= @"_app_id=286bc6f3&_app_key=a175c2748234fcb57b0e4594ea95f3d7";
 static NSString *const kYummlyBaseAPIURL				= @"https://api.yummly.com/v1/api";
 
 #pragma mark - Yummly API Implementation
---
+++ MakeAMealOfIt/YummlyAPIKeys.h
@@ -0,0 +1,16 @@
+//
+//  YummlyAPIKeys.h
+//  MakeAMealOfIt
+//
+//  Created by James Valaitis on 18/04/2014.
+//  Copyright (c) 2014 &Beyond. All rights reserved.
+//
+
+#ifndef MakeAMealOfIt_YummlyAPIKeys_h
+#define MakeAMealOfIt_YummlyAPIKeys_h
+
+static NSString *const kYummlyAppID						= @"286bc6f3";
+static NSString *const kYummlyAppKey					= @"a175c2748234fcb57b0e4594ea95f3d7";
+static NSString *const kYummlyAuthorisationURLExtension	= @"_app_id=286bc6f3&_app_key=a175c2748234fcb57b0e4594ea95f3d7";
+
+#endif
